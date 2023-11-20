5dd132a505ed291a6c51f2e28ec21f64ef2f5907
Andre Miranda <andre@andreldm.com>
2023-11-20 01:47:22 +0100

    Migrate to bootstrap 5

--- js/app.js
+++ js/app.js
@@ -1,43 +1,12 @@
-var map;
-var myCenter = new google.maps.LatLng(53, -1.33);
-var marker = new google.maps.Marker({
-  position: myCenter
-});
+document.addEventListener('show.bs.modal', function (event) {
+  const button = event.relatedTarget;
+  const url = button.getAttribute('href');
+  const target = button.getAttribute('data-bs-target');
 
-function initialize() {
-  var mapProp = {
-    center: myCenter,
-    zoom: 14,
-    draggable: false,
-    scrollwheel: false,
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-  };
-
-  map = new google.maps.Map(document.getElementById('map-canvas'), mapProp);
-  marker.setMap(map);
-
-  google.maps.event.addListener(marker, 'click', function() {
-    infowindow.setContent(contentString);
-    infowindow.open(map, marker);
+  // TODO be a bit smarter, create modal elements on the fly and load external html only
+  fetch(url).then(function(response) {
+    return response.text();
+  }).then(function(body) {
+    document.querySelector('#modal .modal-content').innerHTML = body;
   });
-};
-
-function resizeMap() {
-  if (typeof map != 'undefined') {
-    var center = map.getCenter();
-    google.maps.event.trigger(map, 'resize');
-    map.setCenter(center);
-  }
-}
-
-google.maps.event.addDomListener(window, 'load', initialize);
-google.maps.event.addDomListener(window, 'resize', resizeMap());
-
-$('#modalMapa').on('show.bs.modal', function() {
-  if (typeof map != 'undefined') setTimeout(function() { resizeMap(); }, 400);
-});
-
-// To unload the data when the modal is closed
-$(document).on('hidden.bs.modal', function (e) {
-    $(e.target).removeData('bs.modal');
 });
